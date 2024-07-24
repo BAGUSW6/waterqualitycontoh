00cf762203322d5067be67310d60732aa0362291
BAGUSW6 <127406932+BAGUSW6@users.noreply.github.com>
2024-07-24 10:21:16 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ st.write(
 # Load the data from a CSV. We're caching this so it doesn't reload every time the app reruns.
 @st.cache_data
 def load_data():
-    df = pd.read_csv("data/waterQuality1.csv")
+    df = pd.read_csv("data/ika23_cleaned.csv")
     return df
 
 df = load_data()
@@ -23,8 +23,8 @@ df = load_data()
 # Show a multiselect widget with the chemical parameters using `st.multiselect`.
 parameters = st.multiselect(
     "Parameters",
-    df.columns[:-1],  # Exclude the 'is_safe' column
-    ["aluminium", "ammonia", "arsenic", "barium"]
+    df.columns[3:],  # Exclude the first three columns (Tanggal, Periode, Lokasi)
+    ["PH", "BOD (mg/L)", "COD (mg/L)", "TSS (mg/L)", "DO (mg/L)"]
 )
 
 # Show a slider widget with the rows (for demonstration purposes).
@@ -34,7 +34,7 @@ rows = st.slider("Rows", 1, len(df), (1, 10))
 df_filtered = df.iloc[rows[0]-1:rows[1], :]
 
 # Display the data as a table using `st.dataframe`.
-st.dataframe(df_filtered[parameters + ['is_safe']], use_container_width=True)
+st.dataframe(df_filtered[parameters], use_container_width=True)
 
 # Display the data as an Altair chart using `st.altair_chart`.
 df_chart = pd.melt(
